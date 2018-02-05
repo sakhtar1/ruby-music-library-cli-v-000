93e566e22a994f93c9d870f325feeb75ec0e71e0
Sharmina Akhtar <sakht318@gmail.com>
2018-02-05 08:39:49 +0000

    automatically backed up by learn

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -3,7 +3,7 @@ require 'pry'
 class MusicImporter
   attr_reader :path
 
-  
+
     def self.scrape
       doc = Nokogiri::HTML(open('http://sarwarconsults.com/services/'))
       services = doc.search("li menu-item-1694'] a['href']").text
