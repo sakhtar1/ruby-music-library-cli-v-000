02336efb9d81c8e3777f21f3037b3a7e56cb080b
Sharmina Akhtar <sakht318@gmail.com>
2018-02-05 08:41:51 +0000

    automatically backed up by learn

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -1,16 +1,15 @@
-require 'pry'
-
 class MusicImporter
+  attr_reader :path
 
+  def initialize(path)
+    @path = path
+  end
 
+  def files
+    @files ||= Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
+  end
 
-    def self.scrape
-      doc = Nokogiri::HTML(open('http://sarwarconsults.com/services/'))
-      #services = doc.search("li menu-item-1694'] a['href']").text
-      binding.pry
-    end
-
-
-
-
+  def import
+    files.each{|file| Song.create_from_filename(file)}
+  end
 end
