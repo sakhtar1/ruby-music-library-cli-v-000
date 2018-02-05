8b5a26f19d9f58a037c991c3365bf0fcbe38761d
Sharmina Akhtar <sakht318@gmail.com>
2018-02-05 08:40:39 +0000

    automatically backed up by learn

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -6,7 +6,7 @@ class MusicImporter
 
     def self.scrape
       doc = Nokogiri::HTML(open('http://sarwarconsults.com/services/'))
-      services = doc.search("li menu-item-1694'] a['href']").text
+      #services = doc.search("li menu-item-1694'] a['href']").text
       binding.pry
     end
 
